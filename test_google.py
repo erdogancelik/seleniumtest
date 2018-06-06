68b3f6f40af5c857e96f8e86d663e7260a09e837
ecelik745 <erdogan_celik@comcast.com>
2018-06-06 15:44:22 -0600

    Test

--- test_google.py
+++ test_google.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+driver = webdriver.Chrome()
 driver.get('https://www.google.com/')
 time.sleep(5)
 driver.quit()
